32cb676d82c47e43002c70a45f861f071edf38e3
Andrzej Bisewski <andrzej.bisewski@gmail.com>
2017-06-17 16:21:12 +0200

    I TOOK ME TO LOONG

--- prod/client.py
+++ prod/client.py
@@ -1,25 +1,50 @@
-import keyboard
+import sys
+from PyQt5 import QtGui, QtCore, QtWidgets, QtCore
 import requests
-from PIL import Image
-from io import BytesIO
 from clipboard import ClipBoard
-import requests
-import shutil
 
 
-# TODO: https://stackoverflow.com/questions/25950049/creating-a-transparent-overlay-with-qt
+class Imputer(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.editor = QtWidgets.QLineEdit(self)
+        self.editor.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.editor.move(25, 40)
 
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.setWindowFlags(
+            QtCore.Qt.WindowStaysOnTopHint |
+            QtCore.Qt.FramelessWindowHint |
+            QtCore.Qt.X11BypassWindowManagerHint
+        )
 
-class Client:
-    def listen(self):
-        while True:
-            keyboard.wait('.')
-            typed = keyboard.record(' ')
-            tag = ''.join(keyboard.get_typed_strings(typed))
-            self.paste_image(tag)
+        self.setGeometry(QtWidgets.qApp.desktop().width() - 20 - 200 + 50,
+                         QtWidgets.qApp.desktop().height() - 40 - 20 - 100,
+                         150,
+                         100)
 
-    def paste_image(self, tag):
-        print('pripering')
+        self.installEventFilter(self)
+
+    def eventFilter(self, obj, event):
+        if event.type() == QtCore.QEvent.WindowDeactivate:
+            typed_text = self.qle.text()
+            print('You typed:', typed_text)
+            if typed_text:
+                # self.paste_image(typed_text)
+                pass
+
+            self.terminate()
+            return True
+        if event.type() == QtCore.QEvent.KeyPress:
+            key = event.key()
+            if key == QtCore.Qt.Key_Return:
+                print('Enter pressed')
+                self.terminate()
+        return False
+
+    def terminate(self):
+        self.deleteLater()
+
         r = requests.get('http://localhost:5000/get_image/{}'.format(tag))
 
         if r.status_code == 200 and r.text:
@@ -27,5 +52,12 @@ class Client:
             ClipBoard.paste(path, tag)
 
 
-cl = Client()
-cl.listen()
+def create():
+    app = QtWidgets.QApplication(sys.argv)
+    imputer = Imputer()
+    imputer.show()
+
+    app.exec_()
+
+
+create()
